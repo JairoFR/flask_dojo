3a87c90a1f983fd55762476190c480a335814d08
JairoFR <jfr.elec@gmail.com>
2022-10-30 03:57:13 -0300

    Se termina projecto flask

---
+++ flask_app/docs/scripts.sql
@@ -0,0 +1,57 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema esquema_dojos_y_ninjas
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `esquema_dojos_y_ninjas` ;
+
+-- -----------------------------------------------------
+-- Schema esquema_dojos_y_ninjas
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `esquema_dojos_y_ninjas` DEFAULT CHARACTER SET utf8mb3 ;
+USE `esquema_dojos_y_ninjas` ;
+
+-- -----------------------------------------------------
+-- Table `esquema_dojos_y_ninjas`.`dojos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `esquema_dojos_y_ninjas`.`dojos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `update_at` DATETIME NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 16
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `esquema_dojos_y_ninjas`.`ninjas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `esquema_dojos_y_ninjas`.`ninjas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `dojo_id` INT NOT NULL,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `apellido` VARCHAR(45) NULL DEFAULT NULL,
+  `edad` INT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `update_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_ninjas_dojos_idx` (`dojo_id` ASC) VISIBLE,
+  CONSTRAINT `fk_ninjas_dojos`
+    FOREIGN KEY (`dojo_id`)
+    REFERENCES `esquema_dojos_y_ninjas`.`dojos` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 20
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
